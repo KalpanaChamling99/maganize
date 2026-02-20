f964bbd54c845881a57bb86d3203ab1ea5b4e088
kalpanachamling99 <kalpanachamling99@gmail.com>
2026-02-20 15:39:44 +0545

    change fav icon and add gallery

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -22,6 +22,8 @@ class Admin::ArticlesController < Admin::BaseController
     @article.tag_ids = tag_ids_from_params
 
     if @article.save
+      attach_cover_image
+      attach_gallery_images
       redirect_to admin_articles_path, notice: "Article \"#{@article.title}\" was created."
     else
       set_form_data
@@ -33,6 +35,8 @@ class Admin::ArticlesController < Admin::BaseController
     @article.tag_ids = tag_ids_from_params
 
     if @article.update(article_params)
+      attach_cover_image
+      attach_gallery_images
       redirect_to admin_articles_path, notice: "Article updated."
     else
       set_form_data
@@ -66,7 +70,22 @@ class Admin::ArticlesController < Admin::BaseController
     Array(params.dig(:article, :tag_ids)).reject(&:blank?).map(&:to_i)
   end
 
+  def attach_cover_image
+    cover = params.dig(:article, :cover_image)
+    return unless cover.respond_to?(:read) && cover.original_filename.present?
+    @article.cover_image.attach(cover)
+  end
+
+  def attach_gallery_images
+    raw = params.dig(:article, :images)
+    return if raw.blank?
+    Array(raw).each do |file|
+      next unless file.respond_to?(:read) && file.original_filename.present?
+      @article.images.attach(file)
+    end
+  end
+
   def article_params
-    params.require(:article).permit(:title, :excerpt, :body, :published_at, :featured, :cover_image, :category_id, images: [])
+    params.require(:article).permit(:title, :excerpt, :body, :published_at, :featured, :category_id)
   end
 end
